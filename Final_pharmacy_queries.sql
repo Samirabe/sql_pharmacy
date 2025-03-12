e5005332b690536be176bb0881a1b220f017fbb8
Samirabe <129555311+Samirabe@users.noreply.github.com>
2025-03-12 15:23:06 +0330

    Update Final_pharmacy_queries.sql
    some minor typos

--- Final_pharmacy_queries.sql
+++ Final_pharmacy_queries.sql
@@ -17,9 +17,9 @@ select * from commercial_tbl where company_id in (select company_id from company
 
 select distinct  d.drug_id, d.drug_genericName, c.commercial_name, co.company_name, co.country_id, ct.country_name
 from drug_tbl d
-join commercial_tbl c on d.drug_id = c.drug_id
- join company_tbl co on c.company_id = co.company_id
- join country_tbl ct on co.country_id = ct.country_id
+	join commercial_tbl c on d.drug_id = c.drug_id
+ 	join company_tbl co on c.company_id = co.company_id
+ 	join country_tbl ct on co.country_id = ct.country_id
 where ct.country_name = 'France'
 
 /* result:
@@ -84,7 +84,7 @@ having count(p.prescription_id) > 100;
 /* result:
 Blue Cross			158
 Aetna				174
-UnitedHealthcare	162
+UnitedHealthcare		162
 */
 
 ------------------------------------------------------------------------------------------------------------------
@@ -120,7 +120,7 @@ select * from [dbo].[company_tbl]
 1	Pfizer				1
 2	Sanofi				2
 3	Bayer				4
-4	GlaxoSmithKline		3
+4	GlaxoSmithKline			3
 5	AstraZeneca			3
 6	Apotex				5
 7	Takeda				6
@@ -135,14 +135,14 @@ select * from mostexpensivedrugofcompany(2)
 
 /* to check if it was correct:
 select * from commercial_tbl order by  company_id 
-2	Advil	1	2	10.00
-8	Relenza	1	5	32.00
-1	Amoxil	2	1	15.50
-3	Zoloft	3	3	25.00
-4	Panadol	4	4	8.50
-5	Tamiflu	5	5	30.00
-6	Zyrtec	6	6	12.50
-7	Penicillin-VK	7	7	18.75
+2	Advil	1	2		10.00
+8	Relenza	1	5		32.00
+1	Amoxil	2	1		15.50
+3	Zoloft	3	3		25.00
+4	Panadol	4	4		8.50
+5	Tamiflu	5	5		30.00
+6	Zyrtec	6	6		12.50
+7	Penicillin-VK	7		18.75
 
 */
 
@@ -162,12 +162,12 @@ order by d.drug_id, c.commercial_price;
 /* result
 1	Amoxicillin	1	Antibiotic			1	Amoxil			15.50
 2	Ibuprofen	2	Analgesic			2	Advil			10.00
-3	Sertraline	3	Antidepressant		3	Zoloft			25.00
+3	Sertraline	3	Antidepressant			3	Zoloft			25.00
 4	Paracetamol	2	Analgesic			4	Panadol			8.50
 5	Oseltamivir	4	Antiviral			5	Tamiflu			30.00
 5	Oseltamivir	4	Antiviral			8	Relenza			32.00
-6	Cetirizine	5	Antihistamine		6	Zyrtec			12.50
-7	Penicillin	1	Antibiotic			7	Penicillin-VK	18.75
+6	Cetirizine	5	Antihistamine			6	Zyrtec			12.50
+7	Penicillin	1	Antibiotic			7	Penicillin-VK		18.75
 */
 
 
@@ -192,11 +192,11 @@ where price_rank = 1
 --- I wrote this using cte, u could write it with select tho
 
 /* result of cte
-1	Antibiotic			7	Penicillin	7	Penicillin-VK		18.75
+1	Antibiotic			7	Penicillin	7	Penicillin-VK			18.75
 2	Analgesic			2	Ibuprofen	2	Advil				10.00
-3	Antidepressant		3	Sertraline	3	Zoloft				25.00
+3	Antidepressant			3	Sertraline	3	Zoloft				25.00
 4	Antiviral			5	Oseltamivir	8	Relenza				32.00
-5	Antihistamine		6	Cetirizine	6	Zyrtec				12.50
+5	Antihistamine			6	Cetirizine	6	Zyrtec				12.50
 */
 
 
@@ -214,14 +214,14 @@ order by commercial_name
 Advil				10.3000
 Amoxil				15.9650
 Panadol				8.7550
-Penicillin-VK		19.3125
+Penicillin-VK			19.3125
 Relenza				32.9600
 Tamiflu				30.9000
 Zoloft				25.7500
 Zyrtec				12.8750
 */
 
----easy peasy
+---easy peasy :D
 ------------------- if u want their generic names instead of commercial names:
 
 select d.drug_genericname as drug_name, c.commercial_price * 1.03 as new_price
@@ -254,7 +254,7 @@ order by drug_name
 Advil			10.30
 Amoxil			15.97
 Panadol			8.76
-Penicillin-VK	19.31
+Penicillin-VK		19.31
 Relenza			32.96
 Tamiflu			30.90
 Zoloft			25.75
